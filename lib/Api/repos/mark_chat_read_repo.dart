7e2017934dc22b4d86793f919cf26297c0e712a9
Lokeshsmjangid <lokeshsmofficial@gmail.com>
2025-05-08 12:32:08 +0530

    add an new api chat/mark-read

---
+++ lib/Api/repos/mark_chat_read_repo.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:flipzy/Api/api_constant.dart';
+import 'package:flipzy/Api/api_models/common_model_response.dart';
+import 'package:flipzy/resources/auth_data.dart';
+import 'package:flipzy/resources/utils.dart';
+import 'package:http/http.dart' as http;
+
+Future<CommonModelResponse> addToFavApi({productId}) async {
+  try{
+    final Map<String, dynamic> map = {
+    'userId':AuthData().userModel?.id,
+    'productId':productId,
+    };
+
+
+    flipzyPrint(message: '${ApiUrls.addToFavUrl},$map');
+    http.Response response = await performPostRequest(ApiUrls.addToFavUrl,map);
+    var data = json.decode(response.body);
+
+    if (response.statusCode == 200) {
+      log("${ApiUrls.addToFavUrl}\n $map \n response Start-->\n\n $data \n\n<--response End" );
+      return CommonModelResponse.fromJson(data);
+    } else {
+      handleErrorCases(response, data, ApiUrls.addToFavUrl);
+    }
+  } on SocketException catch (e) {
+    showToastError('No Internet');
+  }
+  catch(e){
+    log('$e');
+    showToastError('$e');
+  }
+  return CommonModelResponse.fromJson({}); // please add try catch to use this
+  // return CommonModelResponse.fromJson(data); // please UnComment to print data and remove try catch
+}
