197a96f5a824ad7204c2f094318c815b3add4eb8
Lokeshsmjangid <lokeshsmofficial@gmail.com>
2025-05-08 13:12:18 +0530

    add pagination param

--- lib/Api/api_models/products_by_category_model.dart
+++ lib/Api/api_models/products_by_category_model.dart
@@ -13,23 +13,39 @@ String productsByCategoryModelResponseToJson(ProductsByCategoryModelResponse dat
 class ProductsByCategoryModelResponse {
   bool? status;
   String? message;
+  int? totalPages;
+  int? totalProducts;
+  int? page;
+  int? limit;
   List<Product>? data;
 
   ProductsByCategoryModelResponse({
     this.status,
     this.message,
+    this.totalPages,
+    this.totalProducts,
+    this.page,
+    this.limit,
     this.data,
   });
 
   factory ProductsByCategoryModelResponse.fromJson(Map<String, dynamic> json) => ProductsByCategoryModelResponse(
     status: json["status"],
     message: json["message"],
+    totalPages: json["totalPages"],
+    totalProducts: json["totalProducts"],
+    page: json["page"],
+    limit: json["limit"],
     data: json["data"] == null ? [] : List<Product>.from(json["data"]!.map((x) => Product.fromJson(x))),
   );
 
   Map<String, dynamic> toJson() => {
     "status": status,
     "message": message,
+    "totalPages": totalPages,
+    "totalProducts": totalProducts,
+    "page": page,
+    "limit": limit,
     "data": data == null ? [] : List<dynamic>.from(data!.map((x) => x.toJson())),
   };
 }
